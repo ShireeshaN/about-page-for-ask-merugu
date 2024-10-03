1ad8ec22cbcd24c1e5add47c9b3810bde1610e95
nandu1-k <121676407+nandu1-k@users.noreply.github.com>
2024-10-03 15:04:50 +0530

    n

--- about.js
+++
@@ -1,6 +0,0 @@
-const mobileMenu = document.getElementById("mobile-menu");
-const navbar = document.querySelector(".navbar");
-
-mobileMenu.addEventListener("click", () => {
-    navbar.classList.toggle("active");
-});
